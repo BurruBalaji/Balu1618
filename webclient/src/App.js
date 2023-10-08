7ddc787cdddcfd1046e0b49ff3af5823e72b9b43
Balaji <burrubalaji@gmail.com>
2023-10-08 12:42:48 +0530

    added second heading in local

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
    <h1>This Heading is added from Balu1618 local repository</h1>
+
+   <h1>This is second Heading  added from Balu1618 local repository</h1>
     </div>
   );
 }
